5ce57f5b6944683ebfd374bb2db2622298854d99
Robert Li <robert.li@wpengine.com>
2021-08-11 08:18:55 +1000

    Initial commit from Create Next App

---
+++ gqless.config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+require('./src/faust.config');
+const { getGqlUrl } = require('@faustjs/core');
+
+/**
+ * @type {import("@gqless/cli").GQlessConfig}
+ */
+const config = {
+  react: false,
+  scalarTypes: { DateTime: 'string' },
+  introspection: {
+    endpoint: getGqlUrl(),
+    headers: {},
+  },
+  destination: './src/client/index.ts',
+  subscriptions: false,
+  javascriptOutput: false,
+};
+
+module.exports = config;
